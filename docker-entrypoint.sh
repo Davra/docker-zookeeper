010a2905387cc5fbfaf0bfc9d3392d8ee5a446be
Gaston Ferrari <gaston.ferrari@davra.io>
2022-08-04 11:27:09 +0000

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -25,21 +25,24 @@ if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
         echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED"
         echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED"
     } >> "$CONFIG"
-    if [[ -z $ZOO_SERVERS ]]; then
-      ZOO_SERVERS="server.1=localhost:2888:3888;2181"
-    fi
-
-    for server in $ZOO_SERVERS; do
-        echo "$server" >> "$CONFIG"
-    done
-
-    if [[ -n $ZOO_4LW_COMMANDS_WHITELIST ]]; then
-        echo "4lw.commands.whitelist=$ZOO_4LW_COMMANDS_WHITELIST" >> "$CONFIG"
-    fi
-
-    for cfg_extra_entry in $ZOO_CFG_EXTRA; do
-        echo "$cfg_extra_entry" >> "$CONFIG"
-    done
+else 
+    CONFIG="/opt/zookeeper/conf/zoo.cfg"
 fi
+if [[ -z $ZOO_SERVERS ]]; then
+    ZOO_SERVERS="server.1=localhost:2888:3888;2181"
+fi
+
+for server in $ZOO_SERVERS; do
+    echo "$server" >> "$CONFIG"
+done
+
+if [[ -n $ZOO_4LW_COMMANDS_WHITELIST ]]; then
+    echo "4lw.commands.whitelist=$ZOO_4LW_COMMANDS_WHITELIST" >> "$CONFIG"
+fi
+
+for cfg_extra_entry in $ZOO_CFG_EXTRA; do
+    echo "$cfg_extra_entry" >> "$CONFIG"
+    done
+
 
 exec "$@"
